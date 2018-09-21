cad1a69788cfe8d73b36895853fe77be93659ed8
andrescass <andres.cass@gmail.com>
2018-09-20 22:58:05 -0300

    models corrected

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -68,7 +68,7 @@ class VideoForm(FlaskForm):
     videoUrl = StringField('Url', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-class EventsForm(FlaskForm): ('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'),
+class EventsForm(FlaskForm): 
     """
     Form for admin to add or edit a event entry
     """
